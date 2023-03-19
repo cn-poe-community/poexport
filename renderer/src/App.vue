d12a2ffd4f100527456631bf7e97ce1698f909c5
hybo <52079867+me1ting@users.noreply.github.com>
2023-03-19 22:08:31 +0800

    feat: add about page

--- renderer/src/App.vue
+++ renderer/src/App.vue
@@ -3,6 +3,7 @@ import Exporter from "./components/Exporter.vue";
 import Translator from "./components/Translator.vue";
 import Database from "./components/Database.vue";
 import Setting from "./components/Setting.vue";
+import About from "./components/About.vue";
 </script>
 
 <template>
@@ -11,22 +12,22 @@ import Setting from "./components/Setting.vue";
       <ul class="actions-container">
         <li
           class="action-item pointer"
-          :class="{ checked: action === 'export' }"
-          @click="switchAction('export')"
+          :class="{ checked: action === Action.EXPORT }"
+          @click="switchAction(Action.EXPORT)"
         >
           <a>BD导出</a>
         </li>
         <li
           class="action-item pointer"
-          :class="{ checked: action === 'translation' }"
-          @click="switchAction('translation')"
+          :class="{ checked: action === Action.TRANSLATION }"
+          @click="switchAction(Action.TRANSLATION)"
         >
           <a>物品翻译</a>
         </li>
         <li
           class="action-item pointer"
-          :class="{ checked: action === 'query' }"
-          @click="switchAction('query')"
+          :class="{ checked: action === Action.QUERY }"
+          @click="switchAction(Action.QUERY)"
         >
           <a>数据库查询</a>
         </li>
@@ -34,34 +35,50 @@ import Setting from "./components/Setting.vue";
       <ul class="actions-container bottom">
         <li
           class="action-item pointer"
-          :class="{ checked: action === 'setting' }"
-          @click="switchAction('setting')"
+          :class="{ checked: action === Action.SETTINGS }"
+          @click="switchAction(Action.SETTINGS)"
         >
           设置
+        </li>
+        <li
+          class="action-item pointer"
+          :class="{ checked: action === Action.ABOUT }"
+          @click="switchAction(Action.ABOUT)"
+        >
+          关于
         </li>
       </ul>
     </div>
     <div class="content">
-      <Exporter v-if="action === 'export'" />
-      <Translator v-else-if="action === 'translation'" />
-      <Database v-else-if="action === 'query'" />
-      <Setting v-else-if="action === 'setting'" />
+      <Exporter v-if="action === Action.EXPORT" />
+      <Translator v-else-if="action === Action.TRANSLATION" />
+      <Database v-else-if="action === Action.QUERY" />
+      <Setting v-else-if="action === Action.SETTINGS" />
+      <About v-else-if="action === Action.ABOUT" />
     </div>
   </main>
 </template>
 
 <script lang="ts">
+enum Action {
+  EXPORT,
+  TRANSLATION,
+  QUERY,
+  SETTINGS,
+  ABOUT,
+}
+
 export default {
   data() {
     return {
-      action: "export",
+      action: Action.EXPORT,
     };
   },
 
   mounted() {},
 
   methods: {
-    switchAction(action: string) {
+    switchAction(action: Action) {
       this.action = action;
     },
   },
---
+++ renderer/src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>About page.</h1>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {},
+};
+</script>
+
+<style scoped></style>
