d5c9b2d0012971e3057de81e40fc25927bac92cd
me1ting <52079867+me1ting@users.noreply.github.com>
2023-12-26 19:11:13 +0800

    Bump cn-poe-export-db to v0.1.4

--- desktop/main.go
+++ desktop/main.go
@@ -13,7 +13,7 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
-const VersionNumber = "1.0.1"
+const VersionNumber = "1.0.2"
 
 func main() {
 	// Create an instance of the app structure
